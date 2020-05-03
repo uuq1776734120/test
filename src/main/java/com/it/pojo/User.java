e8f915c5484c6737a25017aad5e8d261a1912e3a
fyqdudu <1776734120@qq.com>
2020-05-03 19:26:35 +0800

    提交代码

---
+++ src/main/java/com/it/pojo/User.java
@@ -0,0 +1,12 @@
+package com.it.pojo;
+
+/**
+ * @author Jack
+ * @version 1.0
+ * @description com.it.pojo
+ * @date 2020/5/3
+ */
+public class User {
+    private String name;
+    private int age;
+}
